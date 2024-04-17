fea7d2faa36f8df0705de190cdf3a3676a4cb1a5
OyasumiiAlex <b181010@unach.mx>
2024-04-16 23:37:53 -0600

    feature(add): custom table component
    version 1.0.2-alpha

---
+++ src/com/component/table/ActionButtons.java
@@ -0,0 +1,11 @@
+package com.component.table;
+
+import javax.swing.JButton;
+import javax.swing.border.EmptyBorder;
+
+public class ActionButtons extends JButton {
+    public ActionButtons(){
+        setContentAreaFilled(false);
+        setBorder(new EmptyBorder(3,3,3,3));
+    }
+}
