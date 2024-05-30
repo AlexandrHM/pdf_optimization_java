fcb267cbb6ddf6512dc5b6f1b489d092e6b5af3f
OyasumiiAlex <b181010@unach.mx>
2024-05-30 12:01:15 -0600

    feat(style): table and label title
    version 1.0.5-alpha

--- src/com/main/Tab1.java
+++ src/com/main/Tab1.java
@@ -1,9 +1,20 @@
 package com.main;
 
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+import javax.swing.UIManager;
+
 public class Tab1 extends javax.swing.JPanel {
     
     public Tab1() {
         initComponents();
+        //
+        jlabeltitle_tab1.putClientProperty("FlatLaf.styleClass", "h1");
+        //Establece el tema
+        try {
+            UIManager.setLookAndFeel(new FlatMacLightLaf());
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
     }
 
     @SuppressWarnings("unchecked")
@@ -12,7 +23,7 @@ public class Tab1 extends javax.swing.JPanel {
 
         basePanelTab1 = new javax.swing.JPanel();
         jSeparator1 = new javax.swing.JSeparator();
-        jLabel1 = new javax.swing.JLabel();
+        jlabeltitle_tab1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         tableCustom1 = new com.customTable.TableCustom();
         panelSDD1 = new com.customTable.PanelSDD();
@@ -24,13 +35,12 @@ public class Tab1 extends javax.swing.JPanel {
         jSeparator1.setBackground(new java.awt.Color(51, 51, 51));
         jSeparator1.setForeground(new java.awt.Color(0, 0, 0));
 
-        jLabel1.setBackground(new java.awt.Color(51, 51, 51));
-        jLabel1.setFont(new java.awt.Font("Nirmala UI", 1, 14)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(51, 51, 51));
-        jLabel1.setText("Lista de PDF's de cartas responsivas ");
+        jlabeltitle_tab1.setBackground(new java.awt.Color(51, 51, 51));
+        jlabeltitle_tab1.setFont(new java.awt.Font("Nirmala UI", 1, 14)); // NOI18N
+        jlabeltitle_tab1.setForeground(new java.awt.Color(51, 51, 51));
+        jlabeltitle_tab1.setText("Lista de PDF's de cartas responsivas ");
 
-        tableCustom1.setBackground(new java.awt.Color(255, 255, 255));
-        tableCustom1.setForeground(new java.awt.Color(255, 255, 255));
+        tableCustom1.setForeground(new java.awt.Color(51, 51, 51));
         tableCustom1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null, null},
@@ -40,25 +50,33 @@ public class Tab1 extends javax.swing.JPanel {
                 "ID Carta", "Nombre", "Revisión", "ID Empleado", "Fecha Expedición"
             }
         ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
             boolean[] canEdit = new boolean [] {
                 false, false, false, false, false
             };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 return canEdit [columnIndex];
             }
         });
-        tableCustom1.setGridColor(new java.awt.Color(255, 255, 255));
-        tableCustom1.setRowHeight(20);
+        tableCustom1.setGridColor(new java.awt.Color(90, 94, 96));
+        tableCustom1.setRowHeight(30);
         tableCustom1.setSelectionBackground(new java.awt.Color(80, 187, 223));
         tableCustom1.setSelectionForeground(new java.awt.Color(255, 255, 255));
+        tableCustom1.setShowGrid(false);
+        tableCustom1.setShowHorizontalLines(false);
         jScrollPane1.setViewportView(tableCustom1);
         if (tableCustom1.getColumnModel().getColumnCount() > 0) {
             tableCustom1.getColumnModel().getColumn(0).setResizable(false);
             tableCustom1.getColumnModel().getColumn(1).setResizable(false);
             tableCustom1.getColumnModel().getColumn(2).setResizable(false);
             tableCustom1.getColumnModel().getColumn(3).setResizable(false);
-            tableCustom1.getColumnModel().getColumn(4).setResizable(false);
         }
 
         javax.swing.GroupLayout basePanelTab1Layout = new javax.swing.GroupLayout(basePanelTab1);
@@ -68,7 +86,7 @@ public class Tab1 extends javax.swing.JPanel {
             .addGroup(basePanelTab1Layout.createSequentialGroup()
                 .addGap(27, 27, 27)
                 .addGroup(basePanelTab1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 244, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jlabeltitle_tab1, javax.swing.GroupLayout.PREFERRED_SIZE, 450, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(basePanelTab1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                         .addComponent(panelSDD1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGroup(basePanelTab1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -80,14 +98,14 @@ public class Tab1 extends javax.swing.JPanel {
             basePanelTab1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, basePanelTab1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 5, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jlabeltitle_tab1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 11, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(panelSDD1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 522, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 502, Short.MAX_VALUE)
+                .addGap(14, 14, 14))
         );
 
         add(basePanelTab1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1130, 630));
@@ -96,9 +114,9 @@ public class Tab1 extends javax.swing.JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel basePanelTab1;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JLabel jlabeltitle_tab1;
     private com.customTable.PanelSDD panelSDD1;
     private com.customTable.TableCustom tableCustom1;
     // End of variables declaration//GEN-END:variables
